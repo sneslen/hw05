5ed3b2a1f823ec7bd705bf118a1e8c307d03d58e
Sydney Neslen <122399713+sneslen@users.noreply.github.com>
2023-04-18 23:02:22 -0600

    Add files via upload

---
+++ hw05.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+url = 'https://github.com/esnt/Data/raw/main/Names/popular_names.csv'
+
+df = pd.read_csv(url)
+
+st.title('Name Plot')
+
+pattern = st.text_input('Type a name to search', placeholder = 'Enter a name or regular expression.')
+
+name_df = df[df['name']==pattern]
+
+fig = px.scatter(name_df, x='year',y='n', color='sex')
+fig.update_layout(title=f'Name Trend for {pattern} from 1910-2021')
+
+st.plotly_chart(fig)
